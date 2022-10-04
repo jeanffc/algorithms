57e689b722b9b404f5005fda3b42c9f0a6e72d19
Jean Fellipe Ferraz Campos <jeancampos@Jeans-MacBook-Pro.local>
2022-10-04 16:44:15 -0400

    feat: create quick sort algorithm

--- selectionSort.js
+++ selectionSort.js
@@ -1,21 +1,21 @@
 function selectionSort(arr) {
   let min;
 
-  //start passes.
+  // start passes.
   for (let i = 0; i < arr.length; i++) {
-    //index of the smallest element to be the ith element.
+    // index of the smallest element to be the ith element.
     min = i;
 
-    //Check through the rest of the array for a lesser element
+    // check through the rest of the array for a lesser element
     for (let j = i + 1; j < arr.length; j++) {
       if (arr[j] < arr[min]) {
         min = j;
       }
     }
 
-    //compare the indexes
+    // compare the indexes
     if (min !== i) {
-      //swap
+      // swap
       [arr[i], arr[min]] = [arr[min], arr[i]];
     }
   }
