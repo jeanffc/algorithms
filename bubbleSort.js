77cee2a91af332ed2a36ccd4eb2a93b96008f0e7
Jean Fellipe Ferraz Campos <jeancampos@Jeans-MacBook-Pro.local>
2022-10-04 16:10:31 -0400

    feat: change comments

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,12 +1,12 @@
 function bubbleSort(arr) {
-  //Outer pass
+  // Outer pass
   for (let i = 0; i < arr.length; i++) {
-    //Inner pass
+    // Inner pass
     for (let j = 0; j < arr.length - i - 1; j++) {
-      //Value comparison using ascending order
+      // Value comparison using ascending order
 
       if (arr[j + 1] < arr[j]) {
-        //Swapping
+        // Swapping
         [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]];
       }
     }
--- mergeSort.js
+++ mergeSort.js
@@ -1,11 +1,11 @@
-//merging two arrays appropriately.
+// merging two arrays appropriately.
 function merge(arr1, arr2) {
-  //make a new array and have two value pointers
+  // make a new array and have two value pointers
   let res = [],
     i = 0,
     j = 0;
 
-  //sorting the first array.
+  // sorting the first array.
   if (arr1.length > 1) {
     let min = 0;
     for (let i = 0; i < arr1.length; i++) {
@@ -20,7 +20,7 @@ function merge(arr1, arr2) {
     }
   }
 
-  //sorting the second array.
+  // sorting the second array.
   if (arr2.length > 1) {
     let min = 0;
     for (let i = 0; i < arr2.length; i++) {
@@ -35,7 +35,7 @@ function merge(arr1, arr2) {
     }
   }
 
-  //Value comparison.
+  // value comparison.
   while (i < arr1.length && j < arr2.length) {
     if (arr1[i] < arr2[j]) {
       res.push(arr1[i]);
@@ -46,13 +46,13 @@ function merge(arr1, arr2) {
     }
   }
 
-  //pushing the rest of arr1.
+  // pushing the rest of arr1.
   while (i < arr1.length) {
     res.push(arr1[i]);
     i++;
   }
 
-  //pushing the rest of arr2.
+  // pushing the rest of arr2.
   while (j < arr2.length) {
     res.push(arr2[j]);
     j++;
@@ -61,12 +61,12 @@ function merge(arr1, arr2) {
   return res;
 }
 
-//merge sort
+// merge sort
 function mergeSort(arr) {
-  //Best case
+  // best case
   if (arr.length <= 1) return arr;
 
-  //splitting into halves
+  // splitting into halves
   let mid = Math.ceil(arr.length / 2);
 
   let arr1 = arr.slice(0, mid);
@@ -79,12 +79,12 @@ function mergeSort(arr) {
   let arr2_subarrays = [],
     sorted_arr2_subarrays = [];
 
-  //loop through array 1 making subarrays of two elements
+  // loop through array 1 making subarrays of two elements
   for (let i = 0; i < arr1.length; i += 2) {
     arr1_subarrays.push(arr1.slice(i, i + 2));
   }
 
-  //loop through array 2 making subarrays of two elements.
+  // loop through array 2 making subarrays of two elements.
   for (let i = 0; i < arr2.length; i += 2) {
     arr2_subarrays.push(arr2.slice(i, i + 2));
   }
